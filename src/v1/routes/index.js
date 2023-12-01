72ceda0913b4f8ae99a999d78b1b0638382a1e4f
DrJuanch <juanjo88023@gmail.com>
2023-12-01 13:47:42 -0500

    Including index routes

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -1,7 +1,24 @@
-const user = require('./person')
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
-const routes = function (server) {
-  server.use('/user', user);
-}
+const pathRouter = `${__dirname}`;
 
-module.exports = routes;
+const removeExtension = (fileName) => {
+    return fileName.split('.').shift();
+};
+
+fs.readdirSync(pathRouter).filter((file) => {
+    const fileWithOutExt = removeExtension(file);
+    const skip = ['index'].includes(fileWithOutExt);
+    if (!skip) {
+         router.use(`/${fileWithOutExt}`, require(`./${fileWithOutExt}`));
+    }
+});
+
+router.get('*', (req, res) => {
+    res.status(404);
+    res.send({ error: 'Not found route' });
+});
+
+module.exports = router;
