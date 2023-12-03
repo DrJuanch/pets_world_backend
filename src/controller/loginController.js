0f86eff7add76d69ad1f30270857d718951697ba
DrJuanch <juanjo88023@gmail.com>
2023-12-03 15:52:51 -0500

    Solving login problems

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -7,26 +7,29 @@ const error = require('../constansts');
 const loginController = async (req, res) => {
   try {
     const { person_email, person_password } = req.body;
-    const user = await personModel.findOne({ person_email });
+    const user = await personModel.findOne({ person_email: person_email });
 
     if (!user) {
       return response(req, res, error.ERROR_RESPONSES.not_found, 401);
     }
 
+    console.log('click');
     const failedLoginAttempts = user.failedLoginAttempts + 1;
     const comparing = await compare(person_password, user.person_password);
 
     if(comparing){
       if (user.have_sign_in == 0){
         user.have_sign_in += 1;
-        return;
       }
       const haveSignIn = user.have_sign_in;
       res.header('Have-Sign-In', haveSignIn);
       const tokenSession = await tokenSign(user);
       response.success(req, res, {data: user, tokenSession}, 200);
-      user.failedLoginAttempts = 0;
-      user.save();
+      if (user.failedLoginAttempts > 0) {
+        user.failedLoginAttempts = 0;
+        await user.save();
+      }
+      await user.save();
       return
     } else if(!comparing){
       user.failedLoginAttempts = failedLoginAttempts;
--- src/middleware/failedLoginAttemps.js
+++ src/middleware/failedLoginAttemps.js
@@ -1,14 +1,14 @@
-const user = require('../models/personModel');
+const userModel = require('../models/personModel');
 const { compare } = require('../helpers/handleBcrypt');
 const response = require('../helpers/response');
 const error = require('../constansts');
 
 const checkFailedLoginAttempts = async (req, res, next) => {
-  const email = req.body.email;
-  const user = await user.findOne({ person_email: email });
-  if (!user) return response.error(req, res, error.ERROR_RESPONSES.not_found, 401, err);
-  if (user.failedLoginAttempts >= 3){
-    const result = await compare(req.body.password, user.password);
+  const email = req.body.person_email;
+  const foundUser = await userModel.findOne({ person_email: email });
+  if (!foundUser) return response.error(req, res, error.ERROR_RESPONSES.not_found, 401);
+  if (foundUser.failedLoginAttempts >= 3){
+    const result = await compare(req.body.password, foundUser.password);
     if(result == false){
       return response.error(req, res, error.ERROR_RESPONSES.blocked, 401, err);
     };
