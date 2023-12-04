680c0ab9c8b27697286773b98c72c6907dd75129
DrJuanch <juanjo88023@gmail.com>
2023-12-04 11:34:23 -0500

    Role user completed and funcional!

--- src/controller/roleSelectionController.js
+++ src/controller/roleSelectionController.js
@@ -1,22 +1,30 @@
 const personModel = require('../models/personModel');
 const petModel = require('../models/petModel');
 const response = require('../helpers/response');
+const { ERROR_RESPONSES } = require('../constansts');
 
 const selectRoleController = async (req, res) => {
   try {
-    const { selectedRole, email } = req.body;
-    if (!selectedRole || !Object.values(error).includes(selectedRole)) throw new Error("Please provide a valid role");
+    console.log('click');
+    const { role, pet_name, pet_age, email } = req.body;
+    console.log(role);
+    console.log(email);
+    console.log(pet_age);
+    console.log(pet_name);
+    if (!role || !email) {
+      throw new Error("Please provide a valid role");
+    }
 
-    if(selectedRole === 'user'){
+    if (role === 'user') {
       const user = await personModel.findOne({ person_email: email })
-      const { pet_name, pet_age } = req.body;
       const registerPet = await petModel.create({
         pet_owner: user._id,
         pet_name: pet_name,
         pet_age: pet_age
       })
-      await personModel.updateOne({ person_email: email }, { role: selectedRole });
-      response.success(req, res, 'Mascota registrada con éxito' + {data: registerPet}, 200)
+      await personModel.updateOne({ person_email: email }, { role: role });
+      user.save();
+      response.success(req, res, {data: registerPet}, 200)
 
     }
     response.success(req, res, { role: selectedRole }, 200);
