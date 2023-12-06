d2818743e346aec305ac5d3023630e0165d01202
DrJuanch <juanjo88023@gmail.com>
2023-12-06 01:02:06 -0500

    Changing way of reciving images on dog walker module trying to use a new library

--- src/v1/routes/roleSelectionDogWalker.route.js
+++ src/v1/routes/roleSelectionDogWalker.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const upload = multer();
 const { check, validationResult } = require('express-validator');
 const { ERROR_RESPONSES } = require('../../constansts');
 const { selectRoleController } = require('../../controller/roleSelectionController');
